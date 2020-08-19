85c92bb1935d0c90931a1d10f309816c41ddb487
Denise Nanni <denise_n@hotmail.it>
2020-08-19 10:45:32 +0200

    initial commit

---
+++ src/main/java/com/devdenise/devdenisebe/DevdeniseBeApplication.java
@@ -0,0 +1,13 @@
+package com.devdenise.devdenisebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevdeniseBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevdeniseBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devdenise/devdenisebe/DevdeniseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdenise.devdenisebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevdeniseBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
