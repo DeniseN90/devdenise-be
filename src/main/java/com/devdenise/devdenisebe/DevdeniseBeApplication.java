4d29d92eae6e113dfff99d3eb8cb65c6f87ca532
Denise Nanni <denise_n@hotmail.it>
2020-08-25 11:37:05 +0200

    database files and application properties setting added

--- src/main/java/com/devdenise/devdenisebe/DevdeniseBeApplication.java
+++ src/main/java/com/devdenise/devdenisebe/DevdeniseBeApplication.java
@@ -2,8 +2,10 @@ package com.devdenise.devdenisebe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class DevdeniseBeApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/resources/projects.sql
@@ -0,0 +1,4 @@
+INSERT INTO project (id, title, link) VALUES
+(1, 'Leader Clinique','LINK'),
+(2, 'Covid-19Stats', 'LINK'),
+(3, 'Pubbliufficio', 'LINK');
