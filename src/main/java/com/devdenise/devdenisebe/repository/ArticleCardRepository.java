bacb984cf17bcf24422d77292a89e151d15bbde1
Denise Nanni <denise_n@hotmail.it>
2020-08-31 19:00:55 +0200

    ArticleCardRepository modified

--- src/main/java/com/devdenise/devdenisebe/repository/ArticleCardRepository.java
+++ src/main/java/com/devdenise/devdenisebe/repository/ArticleCardRepository.java
@@ -3,5 +3,5 @@ package com.devdenise.devdenisebe.repository;
 import com.devdenise.devdenisebe.model.ArticleCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticlesOutlinesRepository extends JpaRepository<ArticleCard, Long> {
+public interface ArticleCardRepository extends JpaRepository<ArticleCard, Long> {
 }
