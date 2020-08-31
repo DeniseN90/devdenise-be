9ca037f9201e9bb7b44e69772cd4b17c29b7fe9d
Denise Nanni <denise_n@hotmail.it>
2020-09-01 00:11:16 +0200

    fix header

--- src/main/java/com/devdenise/devdenisebe/utils/ControllerUtil.java
+++ src/main/java/com/devdenise/devdenisebe/utils/ControllerUtil.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpHeaders;
 public class ControllerUtil {
 
     public static HttpHeaders getHeaders(){
-        return new HttpHeaders();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccessControlAllowOrigin("https://devdenise.com");
+        return headers;
     }
 }
