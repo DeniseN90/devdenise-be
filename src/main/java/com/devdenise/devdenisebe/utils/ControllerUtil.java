d6b0cd51e49bf8894dbc23caf634222a37f83f98
Denise Nanni <denise_n@hotmail.it>
2020-08-31 19:02:34 +0200

    controller util added

---
+++ src/main/java/com/devdenise/devdenisebe/utils/ControllerUtil.java
@@ -0,0 +1,10 @@
+package com.devdenise.devdenisebe.utils;
+
+import org.springframework.http.HttpHeaders;
+
+public class ControllerUtil {
+
+    public static HttpHeaders getHeaders(){
+        return new HttpHeaders();
+    }
+}
