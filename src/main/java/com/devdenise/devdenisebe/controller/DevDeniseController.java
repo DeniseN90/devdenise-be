36eba487a7e038e0123438b68d1e82af78538b9e
Denise Nanni <denise_n@hotmail.it>
2020-08-31 19:09:22 +0200

    modifica crossorigin

--- src/main/java/com/devdenise/devdenisebe/controller/DevDeniseController.java
+++ src/main/java/com/devdenise/devdenisebe/controller/DevDeniseController.java
@@ -23,7 +23,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = "https://devdenise.com", maxAge = 3600)
 @RestController
 public class DevDeniseController {
 
